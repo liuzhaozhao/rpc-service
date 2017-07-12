9c84a31b02e7c28834e259c7604a0f6abde44f9a
liuzhao <xingkong513@126.com>
2017-07-12 17:13:14 +0800

    提交motan、jupiter的测试

--- src/test/java/rpc/TestClient2.java
+++ src/test/java/rpc/TestClient2.java
@@ -3,9 +3,17 @@ package rpc;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.jupiter.rpc.DefaultClient;
+import org.jupiter.rpc.JClient;
+import org.jupiter.rpc.consumer.ProxyFactory;
+import org.jupiter.rpc.load.balance.LoadBalancerType;
+import org.jupiter.serialization.SerializerType;
+import org.jupiter.transport.JOption;
+import org.jupiter.transport.UnresolvedAddress;
+import org.jupiter.transport.netty.JNettyTcpConnector;
+
 import com.service.rpc.client.ServiceFactory;
 import com.service.rpc.common.JsonUtil;
-import com.service.rpc.serialize.FastJsonSerialize;
 
 import service.Bean;
 import service.DataBean;
@@ -15,11 +23,52 @@ public class TestClient2 {
 	
 	private IService getService() throws InstantiationException, IllegalAccessException {
 		// .setSerialize(new FastJsonSerialize())
-		ServiceFactory.get().setSerialize(new FastJsonSerialize()).init(new String[]{"127.0.0.1:8808","127.0.0.1:8809"});
-		return ServiceFactory.get(IService.class);
+//		ServiceFactory.get().init(new String[]{"127.0.0.1:8808","127.0.0.1:8809"});
+//		return ServiceFactory.get(IService.class);
 		
 //		cn.jugame.http.client.ServiceFactory.ServiceFactory.factory.init(new ServiceSetting("http://localhost:9091", "code", "key").setConnectTimeout(100000).setReadTimeout(100000));
 //		return cn.jugame.http.client.ServiceFactory.ServiceFactory.factory.get(IService.class);
+		
+//		ApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:motan_client.xml");
+//		return (IService) ctx.getBean("service");
+		
+		int processors = Runtime.getRuntime().availableProcessors();// CPU核心数量，用于初始化数据传输渠道
+		// SystemPropertyUtil.setProperty("jupiter.executor.factory.consumer.core.workers",
+		// String.valueOf(processors));
+		// SystemPropertyUtil.setProperty("jupiter.tracing.needed", "false");//
+		// Tracing是否开启(链路跟踪), 默认开启
+		// SystemPropertyUtil.setProperty("jupiter.use.non_blocking_hash",
+		// "true");
+		JClient client = new DefaultClient().withConnector(new JNettyTcpConnector(processors + 1));
+		client.connector().config().setOption(JOption.WRITE_BUFFER_HIGH_WATER_MARK, 512 * 1024);
+		client.connector().config().setOption(JOption.WRITE_BUFFER_LOW_WATER_MARK, 256 * 1024);
+		UnresolvedAddress[] addresses = new UnresolvedAddress[processors];
+		for (int i = 0; i < processors; i++) {
+			addresses[i] = new UnresolvedAddress("127.0.0.1", 18090);
+			client.connector().connect(addresses[i]);// 向服务器建立多个连接
+		}
+		// 连接RegistryServer
+		// client.connectToRegistryServer("127.0.0.1:20001");
+		// 自动管理可用连接
+		// JConnector.ConnectionWatcher watcherIOrder =
+		// client.watchConnections(IOrder.class);
+		// 等待连接可用
+		// if (!watcherIOrder.waitForAvailable(3000)) {
+		// throw new ConnectFailedException();
+		// }
+
+		return ProxyFactory.factory(IService.class).group("testGroup").providerName("testProvider")
+				.version("1.0.0").client(client).serializerType(SerializerType.HESSIAN)
+				.loadBalancerType(LoadBalancerType.ROUND_ROBIN).addProviderAddress(addresses).newProxyInstance();
+		// // 自动管理可用连接
+		// JConnector.ConnectionWatcher watcherIUser =
+		// client.watchConnections(IUser.class);
+		// // 等待连接可用
+		// if (!watcherIUser.waitForAvailable(3000)) {
+		// throw new ConnectFailedException();
+		// }
+		// userService = ProxyFactory.factory(IUser.class)
+		// .version("1.0.0").client(client).newProxyInstance();
 	}
 	
 	/**
@@ -79,4 +128,6 @@ public class TestClient2 {
 		}
 		System.err.println("耗时："+(System.currentTimeMillis() - startTime));
 	}
+	
+	
 }
--- src/test/java/rpc/TestServer.java
+++ src/test/java/rpc/TestServer.java
@@ -1,14 +1,18 @@
 package rpc;
 
+import org.jupiter.common.util.SystemPropertyUtil;
+import org.jupiter.monitor.MonitorServer;
+import org.jupiter.rpc.DefaultServer;
+import org.jupiter.rpc.JServer;
+import org.jupiter.transport.JOption;
+import org.jupiter.transport.netty.JNettyTcpAcceptor;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.service.rpc.exception.RepeatedPathException;
-import com.service.rpc.serialize.FastJsonSerialize;
 import com.service.rpc.server.rpc.RpcServer;
 
-import cn.jugame.http.server.Server;
-import cn.jugame.http.server.auth.SimpleAuth;
+import service.IService;
 import service.Service;
 
 public class TestServer {
@@ -16,24 +20,60 @@ public class TestServer {
 	public void start() throws InstantiationException, IllegalAccessException, RepeatedPathException, InterruptedException {
 //		RpcServer.get().start(8809, TestService.class);
 		// .setSerialize(new FastJsonSerialize())
-		RpcServer.get().setSerialize(new FastJsonSerialize()).start(8808, Service.class);
+		RpcServer.get().start(8808, Service.class);
 		
 	}
 	
 	@org.junit.Test
-	public void startMotan() {
+	public void startMotan() throws InterruptedException {
 		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:motan_server3.xml");
-	}
-	
-	@org.junit.Test
-	public void startHttpServer() throws InterruptedException {
-		Server.start(9091, new SimpleAuth() {
-			@Override
-			public String getKey(String busCode) {
-				return "key";
-			}
-		}, Service.class);
 		System.err.println("服务器已启动");
 		Thread.sleep(1000000);
 	}
+	
+	@org.junit.Test
+	public void startJupiter() throws InterruptedException {
+		// final int processors = Runtime.getRuntime().availableProcessors();
+		// SystemPropertyUtil.setProperty("jupiter.executor.factory.provider.core.workers",
+		// String.valueOf(processors));
+		 SystemPropertyUtil.setProperty("jupiter.metric.needed", "false");//
+		// 是否启用provider的指标度量, 默认启用
+		// SystemPropertyUtil.setProperty("jupiter.metric.csv.reporter",
+		// "false");
+		SystemPropertyUtil.setProperty("jupiter.metric.report.period", "1");
+		// SystemPropertyUtil.setProperty("jupiter.executor.factory.provider.queue.capacity",
+		// "65536");
+		JServer server = new DefaultServer().withAcceptor(new JNettyTcpAcceptor(18090));
+		server.acceptor().configGroup().child().setOption(JOption.WRITE_BUFFER_HIGH_WATER_MARK, 256 * 1024);
+		server.acceptor().configGroup().child().setOption(JOption.WRITE_BUFFER_LOW_WATER_MARK, 128 * 1024);
+		MonitorServer monitor = new MonitorServer();
+		// provider
+		IService service = new Service();
+		// IUser userService = new UserService();
+		monitor.start();
+		// 本地注册
+		// ServiceWrapper provider =
+		server.serviceRegistry().interfaceClass(IService.class).group("testGroup").providerName("testProvider")
+				.version("1.0.0").provider(service)
+				// .provider(userService)
+				.register();
+		// 连接注册中心
+		// server.connectToRegistryServer("127.0.0.1:20001");
+		// 向注册中心发布服务
+		// server.publish(provider);
+		// 启动server
+		server.acceptor().start();
+	}
+	
+//	@org.junit.Test
+//	public void startHttpServer() throws InterruptedException {
+//		Server.start(9091, new SimpleAuth() {
+//			@Override
+//			public String getKey(String busCode) {
+//				return "key";
+//			}
+//		}, Service.class);
+//		System.err.println("服务器已启动");
+//		Thread.sleep(1000000);
+//	}
 }
